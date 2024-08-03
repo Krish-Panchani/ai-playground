2155708627f2ecca33f6899813c70f840369b692
Krish-Panchani <krishpanchani1346@gmail.com>
2024-08-03 22:46:36 +0530

    refactor: Add Login component for handling user authentication

---
+++ src/components/Login.jsx
@@ -0,0 +1,50 @@
+// src/components/Login.jsx
+
+import React from 'react';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { doc, setDoc, getDoc } from 'firebase/firestore';
+import { firestore } from '../firebase'; // Make sure to import firestore
+
+const Login = () => {
+  const auth = getAuth();
+
+  const signInWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      // Create or update user document in Firestore
+      const userDocRef = doc(firestore, 'users', user.uid);
+      const docSnap = await getDoc(userDocRef);
+
+      if (!docSnap.exists()) {
+        console.log("Creating new user document in Firestore");
+        // New user, create document with initial score of 0
+        await setDoc(userDocRef, {
+          displayName: user.displayName,
+          email: user.email,
+          score: 0
+        });
+        console.log("User document created in Firestore:");
+      } else {
+        // Existing user, update score in state
+        console.log("User document exists in Firestore");
+        const docData = docSnap.data();
+        console.log("User document data:", docData);
+        const currentScore = docData.score;
+        console.log("Current score:", currentScore);
+      }
+    } catch (error) {
+      console.error("Error during sign in:", error);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={signInWithGoogle} className='text-black font-semibold px-4 py-2 rounded-full bg-white'>Sign in with Google</button>
+    </div>
+  );
+};
+
+export default Login;
