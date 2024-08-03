b43655e78e3f2b6e5bd293cef0a305b44f62a7cf
Krish-Panchani <krishpanchani1346@gmail.com>
2024-08-03 19:53:22 +0530

    feat: Add GenerateQuestionButton component for generating questions

---
+++ src/components/GenerateQuestionButton.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+
+const GenerateQuestionButton = ({ handleGenerateQuestion, loadingQuestion }) => {
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+  const [remainingTime, setRemainingTime] = useState(0);
+
+  const handleButtonClick = async () => {
+    setIsButtonDisabled(true);
+    setRemainingTime(120); // 2 minutes in seconds
+    await handleGenerateQuestion();
+  };
+
+  useEffect(() => {
+    let timer;
+    if (remainingTime > 0 && isButtonDisabled) {
+      timer = setInterval(() => {
+        setRemainingTime(prevTime => prevTime - 1);
+      }, 1000);
+    } else if (remainingTime === 0) {
+      setIsButtonDisabled(false);
+    }
+
+    // Cleanup interval on component unmount or when timer is done
+    return () => clearInterval(timer);
+  }, [remainingTime, isButtonDisabled]);
+
+  const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    return `${minutes}:${secs < 10 ? `0${secs}` : secs}`;
+  };
+
+  return (
+    <button 
+      onClick={handleButtonClick} 
+      className={isButtonDisabled ? 'text-xl font-semibold px-4 py-2 text-white rounded-lg cursor-not-allowed' : 'bg-black px-4 py-2 text-white font-semibold rounded-full border-2  whitespace-nowrap'}
+      disabled={loadingQuestion || isButtonDisabled}
+    >
+      {loadingQuestion ? 'Loading...' : (isButtonDisabled ? `Retry in ${formatTime(remainingTime)}` : 'Generate Que')}
+    </button>
+  );
+};
+
+export default GenerateQuestionButton;
