720a0723d61f915b005b80bbe3f920249dc9316e
Krish-Panchani <krishpanchani1346@gmail.com>
2024-08-03 19:53:47 +0530

    refactor: Update Score component UI and styling

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Score = ({ score }) => (
   <>
-    <div className='bg-gray-200 px-4 py-2 rounded-xl'>
-      <h2 className='text-lg text-center'>Score: <span className='font-semibold'>{score}</span></h2>
+    <div className='bg-gradient-to-r from-red-500 to-orange-500  px-4 py-2 rounded-full'>
+      <h2 className='text-lg text-center text-white'><span className='font-bold text-xl '>{score}</span> Points</h2>
     </div>
   </>
 );
