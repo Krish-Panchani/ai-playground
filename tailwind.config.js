af362cb93baa5cb39df29757449e77645f7c5300
Krish-Panchani <krishpanchani1346@gmail.com>
2024-08-03 19:54:02 +0530

    refactor: Add keyframes and animation for gradient animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,21 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    keyframes: {
+      'gradient-animate': {
+          '0%, 100%': {
+            'background-size': '200% 100%',
+            'background-position': 'left center'
+          },
+          '50%': {
+            'background-size': '200% 100%',
+            'background-position': 'right center'
+          },
+        },
+    },
+    animation: {
+      'gradient-animate': 'gradient-animate 1s ease infinite',
+    }
   },
   plugins: [],
 }
