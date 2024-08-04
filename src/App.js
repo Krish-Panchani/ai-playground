6250ca3cc9baf697fdc81dc153fa273bc3a74003
Krish-Panchani <krishpanchani1346@gmail.com>
2024-08-04 23:05:53 +0530

    refactor: Add new routes for ArtfulGuesswork and ArtfulStories pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 // import Playground from './pages/Playground';
 import CreativeQuest from './pages/CreativeQuest';
+import ArtfulGuesswork from './pages/ArtfulGuesswork';
+import ArtfulStories from './pages/ArtfulStories';
 import Home from './pages/Home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route index  element={<Home />} />
         <Route path="CreativeQuest" element={<CreativeQuest />} />
+        <Route path="ArtfulGuesswork" element={<ArtfulGuesswork />} />
+        <Route path="ArtfulStories" element={<ArtfulStories />} />
       </Routes>
     </BrowserRouter>
 
