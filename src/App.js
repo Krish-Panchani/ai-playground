a6ddc1e3acfc6f3e93ca7c73c83e4fb915ae58d8
Krish-Panchani <krishpanchani1346@gmail.com>
2024-08-04 15:37:39 +0530

    refactor: Update App.js to use React Router DOM for routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import Playground from './pages/Playground';
+// import Playground from './pages/Playground';
+import CreativeQuest from './pages/CreativeQuest';
+import Home from './pages/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-   <>
-   <Playground />
-   </>
+    <BrowserRouter>
+      <Routes>
+        <Route index  element={<Home />} />
+        <Route path="CreativeQuest" element={<CreativeQuest />} />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
