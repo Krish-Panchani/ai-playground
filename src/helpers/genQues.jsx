d52fa393b8f798efb4ff91fc0bb01f0c199a4e66
Krish-Panchani <krishpanchani1346@gmail.com>
2024-08-04 15:39:19 +0530

    refactor: Update genQues.jsx to generate unique drawing prompts based on age group and skill level

--- src/helpers/genQues.jsx
+++ src/helpers/genQues.jsx
@@ -1,35 +1,21 @@
 import { firebaseApp } from "../firebase";
 import { getVertexAI, getGenerativeModel } from "firebase/vertexai-preview";
 const vertexAI = getVertexAI(firebaseApp);
-const model = getGenerativeModel(vertexAI, { model: "gemini-1.5-flash" });
+const model = getGenerativeModel(vertexAI, { model: "gemini-1.5-flash-001" });
 
 export const handleGenerateQuestion = async (setLoadingQuestion, setQuestion, setPrompt, ageGroup, skillLevel, canvasRef, setResponseText) => {
     setLoadingQuestion(true);
 
     try {
-        const prompt = `
-       Generate a unique drawing prompt suitable for a webpage canvas, based on the user's age group (${ageGroup}) and skill level (${skillLevel}). The prompt should ask the user to draw something appropriate for their age and skill level.
-
-Here are examples for different artist levels:
-
-- **Junior Artist (Age: 12 and below):**
-  - Beginner (Ages 8-10): "Draw the number 7 in a fun and colorful style."
-  - Intermediate (Ages 11-12): "Draw the letter 'A' decorated with patterns or designs."
-
-- **Teen Artist (Ages 13-19):**
-  - Beginner: "Draw a cool, stylized skateboard with some graffiti art."
-  - Intermediate: "Draw a fantasy creature like a dragon or unicorn."
-  - Advanced: "Draw a detailed skateboard park with ramps and rails."
-
-- **Adult Artist (Age 20 and above):**
-  - Beginner: "Draw a geometric pattern using different shapes and colors."
-  - Intermediate: "Draw a scene from your favorite cityscape with architectural details."
-  - Advanced: "Draw a detailed portrait of a person or a character from a story."
-
-Ensure the prompt is unique each time and suitable for the given artist level and skill level.
-Generated Question should be in one Line.
-
-        `;
+      const prompt = `Generate only one easy Question that Ask user to draw something that can be drawn on Canvas of webpage.\n\n
+      based on the user's age group (${ageGroup}) and skill level (${skillLevel}). \n\n
+      It can be Alphabets, Numbers, Shapes, Objects, Animals, Birds, Fruits, Vegetables, Vehicles, etc.\n\n
+      question should be Unique every time. also ask to draw only one object at a time if skill level is Beginner\n\n
+      Example 1. Draw a house.
+      Example 2. Draw a cat.
+      Example 3. Draw a Number 2.
+      `;
+      console.log(prompt);
 
 
         // To generate text output, call generateContent with the text input
