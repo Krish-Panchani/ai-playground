36c5fba70aa7d57db92ad436e0d9e8d18178d963
Krish-Panchani <krishpanchani1346@gmail.com>
2024-08-05 13:39:37 +0530

    refactor: Update Header component to use React Router for navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Score from './Score';
+import { Link } from 'react-router-dom';
 
 const Header = ({ score }) => {
 
   return (
     <>
       <header className="bg-background border-b border-red-600 rounded-full px-4 py-4 md:px-6 flex items-center justify-between">
-    <a  href="/home">
+    <Link to="/">
       <div className='flex items-center gap-2 bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent  animate-gradient-animate'>
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -28,7 +29,7 @@ const Header = ({ score }) => {
       </svg>
       <span className="text-2xl font-bold">AI Playground</span>
       </div>
-    </a>
+      </Link>
     <div className="flex items-center gap-4">
         <Score score={score} />
       <button className="inline-flex items-center justify-center text-white whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-9 rounded-md px-3">
