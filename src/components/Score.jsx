fec3ca30f2444afd115e8863397bbce890d0ddf7
Krish-Panchani <krishpanchani1346@gmail.com>
2024-08-05 18:34:49 +0530

    refactor: Update Score component UI for responsive design

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Score = ({ score }) => (
   <>
-    <div className='bg-gradient-to-r from-red-500 to-orange-500  px-4 py-2 rounded-full'>
+    <div className='bg-gradient-to-r from-red-500 to-orange-500  px-4 py-1 sm:py-2 rounded-full'>
       <h2 className='text-lg text-center text-white'><span className='font-bold text-xl '>{score}</span> Points</h2>
     </div>
   </>
