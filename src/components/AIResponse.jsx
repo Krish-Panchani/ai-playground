4b9c6e3670347e930ac72295572f8ed109c8a2b6
Krish-Panchani <krishpanchani1346@gmail.com>
2024-08-05 13:39:26 +0530

    refactor: Update AIResponse component to include response rendering for different pages

--- src/components/AIResponse.jsx
+++ src/components/AIResponse.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
 
-const AIResponse = ({ loadingResponse, responseText, isCanvasEmpty }) => {
-  const renderResponse = () => {
+const AIResponse = ({ loadingResponse, responseText, isCanvasEmpty, isPage }) => {
+
+  const renderGuessResponse = () => {
+    try{
+      const response = JSON.parse(responseText);
+      const { guess } = response;
+      return (
+        <div className='mt-4 border border-green-300 p-4 rounded-lg text-white'>
+          <p><span className='text-green-600 font-bold'>{guess}</span></p>
+        </div>
+      );
+    }
+    catch (error) {
+      console.error('Error parsing response:', error);
+      return <p>Invalid response format</p>;
+    }
+  };
+  const renderStoryResponse = () => {
+    try{
+      const response = JSON.parse(responseText);
+      const { title, story } = response;
+      return (
+        <div className='mt-4 border border-green-300 p-4 rounded-lg text-white'>
+          <h2>{title}</h2>
+          <p><span className='text-green-600 font-bold'>{story}</span></p>
+        </div>
+      );
+    }
+    catch (error) {
+      console.error('Error parsing response:', error);
+      return <p>Invalid response format</p>;
+    }
+  };
+
+  const renderQueResponse = () => {
     try {
       const response = JSON.parse(responseText);
       const { isCorrect, reason, points } = response;
@@ -13,14 +46,14 @@ const AIResponse = ({ loadingResponse, responseText, isCanvasEmpty }) => {
             <p>Points: +{points}</p>
           </div>
         );
-      }
-      else
+      } else {
         return (
           <div className='mt-4 border border-red-300 p-4 rounded-lg text-white'>
             <p><span className='text-red-600 font-bold'>Incorrect!</span> {reason}</p>
             <p>Points: {points}</p>
           </div>
         );
+      }
     } catch (error) {
       console.error('Error parsing response:', error);
       return <p>Invalid response format</p>;
@@ -30,10 +63,14 @@ const AIResponse = ({ loadingResponse, responseText, isCanvasEmpty }) => {
   return (
     <div className='flex justify-center items-center text-white'>
       {loadingResponse && <p>Loading AI response...</p>}
-      {responseText && !isCanvasEmpty &&(
+      {responseText && !isCanvasEmpty && (
         <div className='mt-4 p-4 rounded-lg'>
           <h3 className='text-xl font-bold bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent'>Gemini - Response</h3>
-          {renderResponse()}
+          
+          {isPage === 'CreativeQuest' && renderQueResponse()}
+          {isPage === 'ArtfulStories' && renderStoryResponse()}
+          {isPage === 'ArtfulGuesswork' && renderGuessResponse}
+
         </div>
       )}
     </div>
