13412e6e683d86aedcad550f47302f85b57368fa
Krish-Panchani <krishpanchani1346@gmail.com>
2024-08-06 17:07:55 +0530

    refactor: Remove commented-out code in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,10 +17,6 @@ const Header = ({ score }) => {
         </Link>
         <div className="flex flex-col sm:flex-row items-center gap-4">
           <Score score={score} />
-          {/* <button className="inline-flex items-center justify-center text-white whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-9 rounded-md px-3">
-            Leaderboard
-          </button> */}
-          
         </div>
         
       </header>
