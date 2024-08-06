2aff0501e704e500a4f6b2458a2c1e98a6af63e7
Krish-Panchani <krishpanchani1346@gmail.com>
2024-08-06 16:06:47 +0530

    refactor: Update Header component UI and remove unused code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,16 @@ import React from 'react';
 import Score from './ui/Score';
 import { Link } from 'react-router-dom';
 import { CgGames } from "react-icons/cg";
-import { HiMenuAlt3 } from "react-icons/hi";
-
 
 const Header = ({ score }) => {
+  
 
   return (
     <>
       <header className="bg-background border-b border-red-600 rounded-full px-4 py-4 md:px-6 flex items-center justify-between">
         <Link to="/">
           <div className='flex items-center gap-2 bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent  animate-gradient-animate'>
-          <CgGames className="w-10 h-10 sm:w-12 sm:h-12 text-orange-500" />
+            <CgGames className="w-10 h-10 sm:w-12 sm:h-12 text-orange-500" />
             <span className="text-xl sm:text-2xl font-bold">AI Playground</span>
           </div>
         </Link>
@@ -21,8 +20,9 @@ const Header = ({ score }) => {
           {/* <button className="inline-flex items-center justify-center text-white whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-9 rounded-md px-3">
             Leaderboard
           </button> */}
-          <HiMenuAlt3 className='text-white text-2xl' />  
+          
         </div>
+        
       </header>
     </>
   );
