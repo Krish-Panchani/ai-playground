bf7ca063d3b2e5333d8d92d8d3e5170e9e7c797e
Krish-Panchani <krishpanchani1346@gmail.com>
2024-08-06 11:29:54 +0530

    refactor: Improve rendering logic and remove unnecessary props in AIResponse component

--- src/components/AIResponse.jsx
+++ src/components/AIResponse.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
-const AIResponse = ({ responseText, isPage }) => {
-
-  const renderGuessResponse = (responseText) => {
+const AIResponse = React.memo(({ responseText, isPage }) => {
+  const renderGuessResponse = useCallback((responseText) => {
     try {
       console.log(responseText);
       const response = JSON.parse(responseText);
@@ -16,9 +15,9 @@ const AIResponse = ({ responseText, isPage }) => {
       console.error('Error parsing response:', error);
       return <p>Invalid response format</p>;
     }
-  };
+  }, []);
 
-  const renderStoryResponse = (responseText) => {
+  const renderStoryResponse = useCallback((responseText) => {
     try {
       const response = JSON.parse(responseText);
       const { title, story } = response;
@@ -32,9 +31,9 @@ const AIResponse = ({ responseText, isPage }) => {
       console.error('Error parsing response:', error);
       return <p>Invalid response format</p>;
     }
-  };
+  }, []);
 
-  const renderQueResponse = (responseText) => {
+  const renderQueResponse = useCallback((responseText) => {
     try {
       const response = JSON.parse(responseText);
       const { isCorrect, reason, points } = response;
@@ -58,22 +57,18 @@ const AIResponse = ({ responseText, isPage }) => {
       console.error('Error parsing response:', error);
       return <p>Invalid response format</p>;
     }
-  };
+  }, []);
 
   return (
     <div className='flex justify-center items-center text-white'>
-      {/* {loadingResponse && <p>Loading AI response...</p>}
-      {responseText && !isCanvasEmpty && ( */}
-        <div className='mt-4 p-4 rounded-lg'>
-          <h3 className='text-xl font-bold bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent'>Gemini - Response</h3>
-          
-          {isPage === 'CreativeQuest' && renderQueResponse(responseText)}
-          {isPage === 'ArtfulStories' && renderStoryResponse(responseText)}
-          {isPage === 'ArtfulGuesswork' && renderGuessResponse(responseText)}
-        </div>
-      {/* )} */}
+      <div className='mt-4 p-4 rounded-lg'>
+        <h3 className='text-xl font-bold bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent'>Gemini - Response</h3>
+        {isPage === 'CreativeQuest' && renderQueResponse(responseText)}
+        {isPage === 'ArtfulStories' && renderStoryResponse(responseText)}
+        {isPage === 'ArtfulGuesswork' && renderGuessResponse(responseText)}
+      </div>
     </div>
   );
-};
+});
 
 export default AIResponse;
