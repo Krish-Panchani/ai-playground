d78ed7a7501beb3bcbb81957847459bb6fd416ae
Krish-Panchani <krishpanchani1346@gmail.com>
2024-08-07 18:35:43 +0530

    corrected typo

--- src/components/ui/Question.jsx
+++ src/components/ui/Question.jsx
@@ -6,7 +6,7 @@ const Question = ({ question }) => (
     <div>
         {question && (
             <div className='flex flex-col md:flex-row px-2 sm:px-8 items-center py-2 rounded-xl'>
-                <p className='text-xl sm:text-3xl font-semibold text-center px-2 underline underline-offset-2 bg-gradient-to-r from-indigo-400 to-cyan-400 bg-clip-text text-transparent'><ReactMarkdown remarkPlugins={[remarkGfm]}>{question}</ReactMarkdown></p>
+                <h3 className='text-xl sm:text-3xl font-semibold text-center px-2 underline underline-offset-2 bg-gradient-to-r from-indigo-400 to-cyan-400 bg-clip-text text-transparent'><ReactMarkdown remarkPlugins={[remarkGfm]}>{question}</ReactMarkdown></h3>
 
             </div>
         )}
--- src/components/ui/Tooltips.jsx
+++ src/components/ui/Tooltips.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 export default function Tooltip({ message, children }) {
     return (
-    <div class="group relative flex">
+    <div className="group relative flex">
         {children}
-        <span class="absolute top-10 scale-0 transition-all rounded bg-gray-800 p-2 text-xs text-white whitespace-nowrap group-hover:scale-100">{message}</span>
+        <span className="absolute top-10 scale-0 transition-all rounded bg-gray-800 p-2 text-xs text-white whitespace-nowrap group-hover:scale-100">{message}</span>
     </div>
     )
 }
