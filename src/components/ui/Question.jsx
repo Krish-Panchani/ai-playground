717eefa212004ab5b4174ed097aa32307345d043
Krish-Panchani <krishpanchani1346@gmail.com>
2024-08-07 14:48:00 +0530

    refactor: Update Question component styling for better responsiveness

--- src/components/ui/Question.jsx
+++ src/components/ui/Question.jsx
@@ -5,9 +5,8 @@ import remarkGfm from 'remark-gfm';
 const Question = ({ question }) => (
     <div>
         {question && (
-            <div className='flex flex-col md:flex-row px-8 items-center py-2 rounded-xl'>
-                {/* <h2 className='text-xl'>AI Generated Que: </h2> */}
-                <p className='text-xl sm:text-3xl font-semibold px-2 underline underline-offset-2 bg-gradient-to-r from-indigo-400 to-cyan-400 bg-clip-text text-transparent'><ReactMarkdown remarkPlugins={[remarkGfm]}>{question}</ReactMarkdown></p>
+            <div className='flex flex-col md:flex-row px-2 sm:px-8 items-center py-2 rounded-xl'>
+                <p className='text-xl sm:text-3xl font-semibold text-center px-2 underline underline-offset-2 bg-gradient-to-r from-indigo-400 to-cyan-400 bg-clip-text text-transparent'><ReactMarkdown remarkPlugins={[remarkGfm]}>{question}</ReactMarkdown></p>
 
             </div>
         )}
