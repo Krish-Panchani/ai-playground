28e76467e1b84d2aadeae194a704b299c612e98f
Krish-Panchani <krishpanchani1346@gmail.com>
2024-08-08 16:39:48 +0530

    refactor: Update UserInfo component to use custom hooks for age, skill, and authentication

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,49 @@ import { motion } from 'framer-motion';
 function Home() {
     const isPage = "Home";
 
+    const links = [
+        {
+            to: "/CreativeQuest",
+            bgImage: "https://res.cloudinary.com/divve6wtz/image/upload/v1722853988/CreativeQuest-removebg-preview_qrlbc3.png",
+            title: "Creative Quest",
+            description: "Where Every Question Sparks a Masterpiece"
+        },
+        {
+            to: "/ArtfulGuesswork",
+            bgImage: "https://res.cloudinary.com/divve6wtz/image/upload/v1722854709/ArtfulGuesswork-removebg-preview_tc7a2u.png",
+            title: "Artful Guesswork",
+            description: "See if AI can identify your creation"
+        },
+        {
+            to: "/ArtfulStories",
+            bgImage: "https://res.cloudinary.com/divve6wtz/image/upload/v1722854711/ArtfulStories-removebg-preview_leuzgt.png",
+            title: "Artful Stories",
+            description: "Where every stroke conjures a magical tale"
+        },
+    ];
+
+    const LinkCard = ({ to, bgImage, title, description }) => (
+        <Link to={to} className='h-[15rem] w-full max-w-[20rem]'>
+            <motion.div
+                whileHover={{ scale: 1.1, rotate: 3 }}
+                whileTap={{ scale: 0.95 }}
+                transition={{ type: 'spring', stiffness: 300 }}
+                className="relative group h-full flex items-end justify-center overflow-hidden rounded-xl text-center text-gray-700 border-2 border-orange-500"
+                style={{ backgroundImage: `url(${bgImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
+            >
+                <div className="absolute inset-0 w-full h-full bg-gradient-to-t from-black/100 via-black/50 to-bg-black group-hover:blur-sm transition duration-500 ease-in-out"></div>
+                <div className="relative p-6 px-6 pt-14 md:px-12">
+                    <h2 className="mb-6 text-2xl font-bold drop-shadow-xl bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent animate-gradient-animate group-hover:text-white transition ease-linear duration-300">
+                        {title}
+                    </h2>
+                    <p className='hidden group-hover:block text-cyan-500 font-semibold'>
+                        {description}
+                    </p>
+                </div>
+            </motion.div>
+        </Link>
+    );
+
     return (
         <div className="flex flex-col gap-4 bg-black p-4">
             <UserInfo isPage={isPage} />
@@ -27,63 +70,11 @@ function Home() {
                     </span>
                 </h3>
             </div>
-            <div className="flex flex-col sm:flex-row justify-center items-center gap-16 py-12">
-                {/* <Link > */}
 
-                <Link to={"/CreativeQuest"} className='h-[15rem] w-full max-w-[20rem]'>
-                    <motion.div
-                        whileHover={{ scale: 1.05 }}
-                        whileTap={{ scale: 0.95 }}
-                        transition={{ type: 'spring', stiffness: 300 }}
-                        className="relative group grid h-[15rem] w-full max-w-[20rem] flex-col items-end justify-center overflow-hidden rounded-xl bg-clip-border text-center text-gray-700 border-2 border-orange-500">
-                        <div
-                            className="absolute inset-0 m-0 h-full w-full overflow-hidden rounded-none bg-transparent bg-[url('https://res.cloudinary.com/divve6wtz/image/upload/v1722853988/CreativeQuest-removebg-preview_qrlbc3.png')] bg-cover bg-clip-border bg-center text-gray-700 shadow-none">
-                            <div className="absolute inset-0 w-full h-full bg-gradient-to-t from-black/100 via-black/50 to-bg-black "></div>
-                        </div>
-                        <div className="relative p-6 px-6 pt-14 md:px-12 ">
-                            <h2 className="mb-6 block font-sans text-xl font-medium leading-[1.5] tracking-normal text-white antialiased">
-                                <span className='text-2xl font-bold drop-shadow-xl bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent  animate-gradient-animate group-hover:text-white transition ease-linear duration-300'>Creative Quest</span>
-                            </h2>
-                            <p className='hidden group-hover:block bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent  animate-gradient-animate font-semibold'>Where Every Question Sparks a Masterpiece</p>
-                        </div>
-                    </motion.div>
-                </Link>
-                <Link to={"/ArtfulGuesswork"} className='h-[15rem] w-full max-w-[20rem]'>
-                    <motion.div
-                        whileHover={{ scale: 1.05 }}
-                        whileTap={{ scale: 0.95 }}
-                        transition={{ type: 'spring', stiffness: 300 }}
-                        className="relative group grid h-[15rem] w-full max-w-[20rem] flex-col items-end justify-center overflow-hidden rounded-xl bg-clip-border text-center text-gray-700 border-2 border-orange-500">
-                        <div
-                            className="absolute inset-0 m-0 h-full w-full overflow-hidden rounded-none bg-transparent bg-[url('https://res.cloudinary.com/divve6wtz/image/upload/v1722854709/ArtfulGuesswork-removebg-preview_tc7a2u.png')] bg-cover bg-clip-border bg-center text-gray-700 shadow-none">
-                            <div className="absolute inset-0 w-full h-full bg-gradient-to-t from-black/100 via-black/50 to-bg-black "></div>
-                        </div>
-                        <div className="relative p-6 px-6 pt-14 md:px-12 ">
-                            <h2 className="mb-6 block font-sans text-xl font-medium leading-[1.5] tracking-normal text-white antialiased">
-                                <span className='text-2xl font-bold drop-shadow-xl bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent  animate-gradient-animate group-hover:text-white transition ease-linear duration-300'>Artful Guesswork</span>
-                            </h2>
-                            <p className='hidden group-hover:block bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent  animate-gradient-animate font-semibold'>See if AI can identify your creation</p>
-                        </div>
-                    </motion.div>
-                </Link>
-                <Link to={"/ArtfulStories"} className='h-[15rem] w-full max-w-[20rem]'>
-                    <motion.div
-                        whileHover={{ scale: 1.05 }}
-                        whileTap={{ scale: 0.95 }}
-                        transition={{ type: 'spring', stiffness: 300 }}
-                        className="relative group grid h-[15rem] w-full max-w-[20rem] flex-col items-end justify-center overflow-hidden rounded-xl bg-clip-border text-center text-gray-700 border-2 border-orange-500">
-                        <div
-                            className="absolute inset-0 m-0 h-full w-full overflow-hidden rounded-none bg-transparent bg-[url('https://res.cloudinary.com/divve6wtz/image/upload/v1722854711/ArtfulStories-removebg-preview_leuzgt.png')] bg-cover bg-clip-border bg-center text-gray-700 shadow-none">
-                            <div className="absolute inset-0 w-full h-full bg-gradient-to-t from-black/100 via-black/50 to-bg-black "></div>
-                        </div>
-                        <div className="relative p-6 px-6 pt-14 md:px-12 ">
-                            <h2 className="mb-6 block font-sans text-xl font-medium leading-[1.5] tracking-normal text-white antialiased">
-                                <span className='text-2xl font-bold drop-shadow-xl bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent  animate-gradient-animate group-hover:text-white transition ease-linear duration-300'>Artful Stories</span>
-                            </h2>
-                            <p className='hidden group-hover:block bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent  animate-gradient-animate font-semibold'>Where every stroke conjures a magical tale</p>
-                        </div>
-                    </motion.div>
-                </Link>
+            <div className="flex flex-col sm:flex-row justify-center items-center gap-16 py-12">
+                {links.map((link, index) => (
+                    <LinkCard key={index} {...link} />
+                ))}
             </div>
         </div>
     )
