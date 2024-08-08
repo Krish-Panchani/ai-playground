690ec79c94683ddb06ede8636bed91980465cdeb
Krish-Panchani <krishpanchani1346@gmail.com>
2024-08-08 18:13:26 +0530

    refactor: Update Leaderboard page heading and add description

--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { MdOutlineLeaderboard } from "react-icons/md";
 
 import UserInfo from '../components/UserInfo';
 import LeaderboardTable from '../components/ui/LeaderboardTable';
@@ -51,9 +50,9 @@ function Leaderboard() {
         <div className="flex flex-col min-h-screen bg-black p-4">
             <UserInfo isPage={isPage} />
             <div className="w-full max-w-4xl text-white mx-auto p-2 md:p-10 h-screen">
-                <div className="flex items-center gap-2 justify-center mb-6">
-                    <MdOutlineLeaderboard className='text-3xl' />
-                    <h1 className="text-2xl text-center font-bold bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent animate-gradient-animate">Leaderboard</h1>
+                <div className="flex flex-col items-center gap-4 justify-center mb-6">
+                    <h1 className="text-5xl text-center font-bold bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent animate-gradient-animate">Leaderboard</h1>
+                    <p>See where you stand among the best.</p>
                 </div>
                 <LeaderboardTable users={users} />
                 {user && (
