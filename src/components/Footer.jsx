0113a0ed5fff43e220cc5f8f320c9c8196a620e0
Krish-Panchani <krishpanchani1346@gmail.com>
2024-08-08 16:44:43 +0530

    refactor: Update Footer component to include link to homepage

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Footer() {
     return (
         <footer className='bg-background border-t border-red-600 rounded-full px-4 py-4 md:px-6 flex items-center justify-between'>
-            <p className='font-semibold text-white text-center mx-auto'>© 2024 <span className='bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent font-bold'>AI Playground</span> - Made with Love by <span className='bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent font-bold'>Krish Panchani</span></p>
+            <p className='font-semibold text-white text-center mx-auto'>© 2024 {' '}
+                <Link to={"/"}>
+                    <span className='bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent font-bold'>AI Playground
+                        </span> - {' '}
+                </Link>
+                Made with Love by {' '}
+                <span className='bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent font-bold'>Krish Panchani</span>
+            </p>
         </footer>
     )
 }
